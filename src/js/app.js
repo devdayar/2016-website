f8b157e8356cf843a01e19225d2168ac258023b7
cristhian <cris@acamica.com>
2016-11-01 22:04:40 -0300

    Add menu

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,88 @@
 'use strict'
 
+// --------------------------------------------------
+//      Auxiliar functions
+// --------------------------------------------------
+
+const animateScroll = (target, duration, easing, padding, align, onFinish) => {
+  padding = padding || 0
+  var docElem = document.documentElement
+  var windowHeight = docElem.clientHeight
+  var maxScroll = ('scrollMaxY' in window) ? window.scrollMaxY : (docElem.scrollHeight - windowHeight)
+  var currentY = window.pageYOffset
+
+  var targetY = currentY
+  var elementBounds = isNaN(target) ? target.getBoundingClientRect() : 0
+
+  if (align === 'center') {
+    targetY += isNaN(target) ? (elementBounds.top + elementBounds.height / 2) : target
+    targetY -= windowHeight / 2
+    targetY -= padding
+  } else if (align === 'bottom') {
+    targetY += elementBounds.bottom || target
+    targetY -= windowHeight
+    targetY += padding
+  } else {
+    targetY += elementBounds.top || target
+    targetY -= padding
+  }
+  targetY = Math.max(Math.min(maxScroll, targetY), 0)
+
+  var deltaY = targetY - currentY
+
+  var obj = {
+    targetY: targetY,
+    deltaY: deltaY,
+    duration: duration || 0,
+    easing: (easing in animateScroll.Easing) ? animateScroll.Easing[easing] : animateScroll.Easing.linear,
+    onFinish: onFinish,
+    startTime: Date.now(),
+    lastY: currentY,
+    step: animateScroll.step
+  }
+
+  window.requestAnimationFrame(obj.step.bind(obj))
+}
+
+animateScroll.Easing = {
+  linear: function (t) { return t },
+  easeInQuad: function (t) { return t * t },
+  easeOutQuad: function (t) { return t * (2 - t) },
+  easeInOutQuad: function (t) { return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t },
+  easeInCubic: function (t) { return t * t * t },
+  easeOutCubic: function (t) { return (--t) * t * t + 1 },
+  easeInOutCubic: function (t) { return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1 },
+  easeInQuart: function (t) { return t * t * t * t },
+  easeOutQuart: function (t) { return 1 - (--t) * t * t * t },
+  easeInOutQuart: function (t) { return t < 0.5 ? 8 * t * t * t * t : 1 - 8 * (--t) * t * t * t },
+  easeInQuint: function (t) { return t * t * t * t * t },
+  easeOutQuint: function (t) { return 1 + (--t) * t * t * t * t },
+  easeInOutQuint: function (t) { return t < 0.5 ? 16 * t * t * t * t * t : 1 + 16 * (--t) * t * t * t * t }
+}
+
+animateScroll.step = function () {
+  if (this.lastY !== window.pageYOffset && this.onFinish) {
+    this.onFinish()
+    return
+  }
+
+  var t = Math.min((Date.now() - this.startTime) / this.duration, 1)
+
+  var y = this.targetY - ((1 - this.easing(t)) * (this.deltaY))
+  window.scrollTo(window.scrollX, y)
+
+  if (t !== 1) {
+    this.lastY = window.pageYOffset
+    window.requestAnimationFrame(this.step.bind(this))
+  } else {
+    if (this.onFinish) this.onFinish()
+  }
+}
+
+// --------------------------------------------------
+//      Asynchronous image loading process
+// --------------------------------------------------
+
 function aload () {
   const attribute = 'data-aload'
   let nodes = window.document.querySelectorAll('[' + attribute + ']')
@@ -17,3 +100,47 @@ function aload () {
 }
 
 aload()
+
+// --------------------------------------------------
+//      Menu
+// --------------------------------------------------
+
+const menu = document.querySelector('#menu')
+const menuLinks = menu.querySelectorAll('a')
+const menuToggle = document.querySelector('#menu-toggle')
+const nav = document.querySelector('#nav')
+
+const toggleMenu = () => {
+  menu.classList.toggle('dn')
+  menu.classList.toggle('db')
+  menu.classList.toggle('fadeIn')
+
+  if (menuToggle.classList.contains('is-active')) {
+    menuToggle.textContent = '☰ Menú'
+  } else {
+    menuToggle.textContent = '✖ Cerrar'
+  }
+
+  menuToggle.classList.toggle('is-active')
+
+  nav.classList.toggle('bg-black-40')
+  nav.classList.toggle('bg-black-90')
+}
+
+menuToggle.addEventListener('click', e => {
+  e.preventDefault()
+
+  toggleMenu()
+})
+
+Array.prototype.forEach.call(menuLinks, link => {
+  link.addEventListener('click', e => {
+    e.preventDefault()
+
+    const elementToScrollTo = document.querySelector(e.currentTarget.getAttribute('href'))
+
+    toggleMenu()
+
+    animateScroll(elementToScrollTo, 1000, 'easeInOutQuint', nav.offsetHeight)
+  })
+})
